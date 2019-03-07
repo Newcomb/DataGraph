38f892a95c9206213bd0b0ebaee3c91f2df23ad3
Newcomb <47224717+Newcomb@users.noreply.github.com>
2019-03-07 11:57:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ var makeChart = function(data)
      .enter()
      .append("text")
      .attr("x",function(d,i){return .9*width;})
-     .attr("y",function(d,i){return i*20+5;})
+     .attr("y",function(d,i){return i*20+7;})
      .attr("width",20)
      .attr("height",10)
      .text(function(d){return d.state;});
